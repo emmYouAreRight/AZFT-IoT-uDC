5d3c8562239f825f9d7cb059fc52679c1d6f4a96
daixinye <denight@qq.com>
2019-02-27 11:16:22 +0800

    remove unused files

--- server/routes/route.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-// router api see https://www.npmjs.com/package/koa-router
-//
-module.exports = function(router, app) {
-    router.get('/index', async (ctx, next) => {
-        await ctx.render('index', {title: 'hello'});
-        await next();
-    });
-};
--- server/routes/training-route.js
+++
@@ -1,8 +0,0 @@
-'use static';
-module.exports = function(router, app) {
-    router.prefix('/training')
-        .get('/index', async function(ctx, next) {
-            await ctx.render('training/index');
-            await next();
-        });
-}
